0dd85546951ad031e6a3e5e06565391c1cb8b3e9
Coen Koopmans <info@coenkoopmans.nl>
2020-10-30 22:50:46 +0100

    ci: updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "www.dordtfilm.nl" > build/CNAME
+echo "www.dordtfilm.nl" > dist/CNAME
    angular-cli-ghpages --branch deploy --no-silent \
-   --repo="https://$GITHUB_TOKEN@github.com/FastWorks-Studio/dordtfilm.nl.git" \
+   --repo="https://\$GITHUB_TOKEN@github.com/FastWorks-Studio/dordtfilm.nl.git" \
    --name="Automatic deployment" --email="info@fastworks-studio.nl"
