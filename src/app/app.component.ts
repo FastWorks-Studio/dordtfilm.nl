053c692be57e902749b9d45ee4799df11593288b
Kevin H <k.vandenhoek@wearetriple.com>
2020-10-31 16:55:32 +0100

    stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,18 @@ export class AppComponent {
   title = "Dordrecht";
   subtitle = "door de jaren heen";
   buttonText = "Koop nu kaartjes";
+
+  public scrollDown(): void {
+    const introElement: HTMLElement = document.getElementsByClassName(
+      "intro"
+    )[0] as HTMLElement;
+    const wrapperElement: HTMLElement = document.getElementsByClassName(
+      "wrapper"
+    )[0] as HTMLElement;
+    wrapperElement.scroll({
+      top: introElement.offsetHeight,
+      left: window.scrollX,
+      behavior: "smooth",
+    });
+  }
 }
