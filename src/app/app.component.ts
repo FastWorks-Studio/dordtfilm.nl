54a2c598af1a6988700f7e24ebe1c447a10645d0
Coen Koopmans <info@coenkoopmans.nl>
2020-10-31 18:31:36 +0100

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,26 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "Dordrecht";
-  subtitle = "door de jaren heen";
-  buttonText = "Koop nu kaartjes";
+  public title: string = 'Dordrecht';
+  public subtitle: string = 'door de jaren heen';
+  public buttonText: string = 'Koop nu kaartjes';
 
   public scrollDown(): void {
     const introElement: HTMLElement = document.getElementsByClassName(
-      "intro"
+      'intro'
     )[0] as HTMLElement;
     const wrapperElement: HTMLElement = document.getElementsByClassName(
-      "wrapper"
+      'wrapper'
     )[0] as HTMLElement;
     wrapperElement.scroll({
       top: introElement.offsetHeight,
       left: window.scrollX,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   }
 }
