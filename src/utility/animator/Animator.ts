a8e5019f3b7d5b4d5299f66f058fea20d232abf2
Kevin H <k.vandenhoek@wearetriple.com>
2020-11-07 03:30:06 +0100

    cleanup

--- src/utility/animator/Animator.ts
+++ src/utility/animator/Animator.ts
@@ -1,3 +1,4 @@
+import { cleanup } from '@testing-library/react';
 import * as Model from '../../models/module';
 import * as Utility from '../../utility/module';
 
@@ -14,64 +15,66 @@ export class Animator {
         this.startProperties = animation.from?.properties || Model.Transform.identity.properties;
         this.endProperties = animation.to?.properties || Model.Transform.identity.properties;
         if (Utility.exists(animation.delay)) {
-            this.renderState();
+            this.renderState(0);
             setInterval(this.renderNextFrame.bind(this), ((animation.delay || 0) * 1000))
         } else {
-            this.renderState();
             this.renderNextFrame()
         }
     }
 
-    private element: HTMLElement;
+    private element: HTMLElement | null;
     private startProperties: Model.TransformProperties;
     private endProperties: Model.TransformProperties;
     private animation: Model.Animation;
 
-    private elapsed: number = 0;
-    private frameDuration: number = 0.02;
-
-    private get progress() { return this.elapsed / this.animation.duration; }
+    private previousUpdate: number = Date.now();
+    private startTime: number = Date.now();
 
     private renderNextFrame() {
-        this.elapsed = Math.min(this.elapsed + this.frameDuration, this.animation.duration);
-        this.renderState();
-        if (this.elapsed < this.animation.duration) { requestAnimationFrame(this.renderNextFrame.bind(this)); }
+        const progress = (this.previousUpdate - this.startTime) / this.animation.duration;
+        this.renderState(progress);
+        if (progress < 1) { 
+            requestAnimationFrame(this.renderNextFrame.bind(this)); 
+        } else {
+            this.element = null;
+        }
     }
 
-    private renderState() {
-        this.renderOpacity();
-        this.renderTransform();
+    private renderState(progress: number) {
+        this.renderOpacity(progress);
+        this.renderTransform(progress);
     }
 
-    private renderOpacity() {
-        const value = this.interpolate({ from: this.startProperties.opacity, to: this.endProperties.opacity, fallback: 1 })
-        if (Utility.isVoid(value)) { return; }
-        this.element.style.opacity = value?.toFixed(2) || ``;
+    private renderOpacity(progress: number) {
+        const value = this.interpolate({ from: this.startProperties.opacity, to: this.endProperties.opacity, progress: progress, fallback: 1 })
+        if (Utility.isVoid(value) || this.element === null || this.element === undefined) { return; }
+        this.element.style.opacity = value?.toFixed(4) || ``;
     }
 
-    private renderTransform() {
-        const scale = this.getScale();
-        const rotation = this.getRotation();
-        const translation = this.getTranslation();
+    private renderTransform(progress: number) {
+        if (this.element === null || this.element === undefined) { return; }
+        const scale = this.getScale(progress);
+        const rotation = this.getRotation(progress);
+        const translation = this.getTranslation(progress);
         let translations: string[] = []
-        if (scale !== null) { translations.push(`scale(${scale.toFixed(5)})`); }
-        if (rotation !== null) { translations.push(`rotate(${rotation.toFixed(2)}deg)`); }
-        if (translation !== null) { translations.push(`translate3d(${translation.x.toFixed(5)}px, ${translation.y.toFixed(5)}px, 0px)`); }
+        if (scale !== null) { translations.push(`scale(${scale.toFixed(4)})`); }
+        if (rotation !== null) { translations.push(`rotate(${rotation.toFixed(4)}deg)`); }
+        if (translation !== null) { translations.push(`translate3d(${translation.x.toFixed(4)}px, ${translation.y.toFixed(4)}px, 0px)`); }
         const transform = translations.join(" ");
         this.element.style.transform = transform;
     }
 
-    private getScale(): number | null {
-        return this.interpolate({ from: this.startProperties.scale, to: this.endProperties.scale, fallback: 1 })
+    private getScale(progress: number): number | null {
+        return this.interpolate({ from: this.startProperties.scale, to: this.endProperties.scale, progress: progress, fallback: 1 })
     }
 
-    private getRotation(): number | null {
-        return this.interpolate({ from: this.startProperties.rotation, to: this.endProperties.rotation, fallback: 0 })
+    private getRotation(progress: number): number | null {
+        return this.interpolate({ from: this.startProperties.rotation, to: this.endProperties.rotation, progress: progress, fallback: 0 })
     }
 
-    private getTranslation(): { x: number, y: number } | null {
-        const x = this.interpolate({ from: this.startProperties.translation?.x, to: this.endProperties.translation?.x, fallback: 0 })
-        const y = this.interpolate({ from: this.startProperties.translation?.y, to: this.endProperties.translation?.y, fallback: 0 })
+    private getTranslation(progress: number): { x: number, y: number } | null {
+        const x = this.interpolate({ from: this.startProperties.translation?.x, to: this.endProperties.translation?.x, progress: progress, fallback: 0 })
+        const y = this.interpolate({ from: this.startProperties.translation?.y, to: this.endProperties.translation?.y, progress: progress, fallback: 0 })
         if (x === null && y === null) { return null; }
         return { 
             x: Utility.unwrap<number>({ optional: x, fallback: 0 }),
@@ -79,11 +82,11 @@ export class Animator {
         }
     }
 
-    private interpolate(args: {from?: number, to?: number, fallback?: number }): number | null {
+    private interpolate(args: {from?: number, to?: number, progress: number, fallback?: number }): number | null {
         if (Utility.isVoid(args.from) && Utility.isVoid(args.to)) { return null; }
         const start = Utility.unwrap({ optional: args.from, fallback: Utility.unwrap({ optional: args.fallback, fallback: 0 }) })
         const end = Utility.unwrap({ optional: args.to, fallback: Utility.unwrap({ optional: args.fallback, fallback: 0 }) })
-        const progress = this.animation.curve?.value(this.progress) || this.progress;
+        const progress = this.animation.curve?.value(args.progress) || args.progress;
         const value = ((1 - progress) * start) + (progress * end);
         return value;
     }
